8d9f124647511ca77a52105f74f809d4d31952c3
valdearg <tim11pop@hotmail.com>
2021-09-12 17:11:05 +0100

    Using reference expansion now

--- get_title.py
+++ get_title.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 from replacements import check_replacements
 from blocked_groups import check_blocked_group
@@ -12,14 +13,14 @@ def func_get_chapter_name(chapter_id):
     }
 
     chapter_data = requests.get(
-        url=f"https://api.mangadex.org/chapter/{chapter_id}", headers=head).json()
+        url=f"https://api.mangadex.org/chapter/{chapter_id}?includes[]=scanlation_group&includes[]=manga", headers=head).json()
 
     volume_num = chapter_data["data"]["attributes"]["volume"]
     chapter_num = chapter_data["data"]["attributes"]["chapter"]
     chapter_title = chapter_data["data"]["attributes"]["title"]
     published_at = chapter_data["data"]["attributes"]["publishAt"]
 
-    for i in chapter_data["relationships"]:
+    for i in chapter_data["data"]["relationships"]:
 
         if i['type'] == "scanlation_group":
 
@@ -30,33 +31,10 @@ def func_get_chapter_name(chapter_id):
             if is_blocked_group is True:
                 return None, None
 
-            is_known_group = check_known_group(group_id)
-
-            if is_known_group is True:
-                return None, None
-
-            if is_known_group:
-                group_name = is_known_group
-            else:
-                try:
-                    groups = requests.get(
-                        url=f"https://api.mangadex.org/group/{group_id}", headers=head).json()
-
-                    group_name = groups["data"]["attributes"]["name"]
-
-                    add_known_group(group_id, group_name)
-                except Exception as ex:
-                    print("Error obtaining group ID:", group_id)
-                    group_name = "UNKNOWN"
+            group_name = i["attributes"]["name"]
 
         if i['type'] == "manga":
-
-            manga_id = i['id']
-
-            manga = requests.get(
-                url=f"https://api.mangadex.org/manga/{manga_id}", headers=head).json()
-
-            manga_name = manga["data"]["attributes"]["title"]["en"]
+            manga_name = i["attributes"]["title"]['en']
 
     if manga_name:
         manga_name = check_replacements(manga_name)
